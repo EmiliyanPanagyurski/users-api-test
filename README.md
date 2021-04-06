2e7f4402a9a4dfd9233655949e1ce86fa90c6ca5
Emiliyan Panagyurski <emiliyan.panagyurski@techhuddle.com>
2021-04-06 14:31:34 +0300

    sample api for testing auth0 integrations

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # users-api-test
+
+To start run command npm run start.
+
+Create .env file and use env.example to see what fields yoyu need to have.
+
+In file users.json add the users you wish to have. Inside the file there is a exaple user.
+When ever you make request to the endpoints it will check the users in users.json.
+
+To expose local api use ngrok.
---
+++ server.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const bodyParser = require('body-parser');
+const port = 3001;
+const users = require('./users.json');
+
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${process.env.AUTH0_TENANT}.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: `${process.env.AUTH0_API_AUDIENCE}`,
+  issuer: `${process.env.AUTH0_TENANT}`,
+  algorithms: ['RS256']
+});
+
+app.use(bodyParser.json({ extended: true }));
+
+app.post('/api/authentivate/user', checkJwt, function (req, res) {
+  const user = users.find(u => u.email === req.body.email);
+  if (user) {
+    if (user.password === req.body.password) {
+      res.status(200).json({
+        name: user.username,
+        id: Math.random(),
+        email: user.email
+      });
+    }
+  } else {
+    res.status(404).json({
+      message: 'User not foundcccc.'
+    });
+  }
+});
+
+app.get('/api/users/:email', checkJwt, function (req, res) {
+  const user = users.find(u => u.email === req.body.email);
+
+  if (user) {
+    res.status(200).json({
+      nickname: 'username',
+      email: req.params.email
+    });
+  } else {
+    res.status(404).json({
+      message: `User with the email address ${req.params.email} not found.`
+    });
+  }
+});
+
+app.get('/security/api/v1/users/:email/accesscontrol/:organisation_id', function (req, res) {
+  const user = users.find(u => u.email === req.params.email && u.organisation_id == req.params.organisation_id);
+
+  if (user) {
+    res.status(200).json({
+      "hasAccessToProduct": user.hasAccessToProduct,
+      "isEnabledAgainstProduct": user.isEnabledAgainstProduct,
+      "roles": user.roles,
+      "productUserId": user.productUserId,
+    });
+  } else {
+    res.status(404).json({
+      message: `User with the email address ${req.params.email} not found.`
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
